b7cf1c2a87cf7fbe1069cfaf3c33d20aa4fe0fd0
Lelihelija <lelihelija@gmail.com>
2021-04-10 14:35:44 +0300

    remove proc file

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,6 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      // target: 'http://localhost:' + (process.env.SERVER_PORT || 5000),
       target: process.env.SERVER_PROXY || 'http://localhost:5000',
       changeOrigin: true,
     })
